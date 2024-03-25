1d677f5b7b4cb1c02c4cab8bfa907c3d384ee9ee
babu <csebabu3@gmail.com>
2024-03-25 15:49:46 +0530

    werf

---
+++ src/Component/Topnav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Topnav() {
+    return (
+        <div className='container'>
+            <nav class="navbar navbar-light bg-light">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">My Smart Basket</a>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Topnav
---
+++ src/Component/Vivimart.js
@@ -0,0 +1,183 @@
+import React from 'react';
+import '../App.css';
+
+function Vivimart() {
+    return (
+        <div className='container mt-3 bg-light'>
+            <div className="row row-cols-1 row-cols-md-3 g-4">
+                <div className="col-md-3 shadow">
+                    <div className="card h-100">
+                        <div className='relative'>
+                            <img src="https://t4.ftcdn.net/jpg/05/37/04/61/360_F_537046123_s8JVn2NrClPQDOryhSm8jonYZPfIzPRX.jpg" className="card-img-top" alt="..." />
+                            <div className='absolute'>
+                                <button className='btn'>41% OFF</button>
+                                <div>
+                                    <input type='radio'></input>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='desc'>
+                            <button className='patch'><i class="fa fa-bicycle" aria-hidden="true"></i> 2hrs</button>
+                            <p>Fresho</p>
+                            <h5>Capcium - (loose)</h5>
+                            <select className="form-select mt-5" aria-label="Default select example">
+                                <option selected>1 Kg</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                            <div className='d-flex gap-2 mt-2'>
+                                <h4><i class="fa fa-inr" aria-hidden="true"></i> 68</h4>
+                                <p className='mt-1' style={{ textDecoration: " line-through" }}><i class="fa fa-inr" aria-hidden="true"></i> 68790</p>
+                            </div>
+                            <select className="form-select mt-4" aria-label="Default select example">
+                                <option selected>Har Din Sasta!</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                            <div className='d-flex'>
+                                <span style={{ border: '1px solid lightgrey', padding: '8px', marginTop: "8px" }}>
+                                    <i className="fa fa-bookmark-o" aria-hidden="true"></i>
+                                </span>
+                                <button className='btn1'>Add</button>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="col-md-3 shadow">
+                    <div className="card h-100">
+                        <div className='relative'>
+                            <img src="https://farmersfz.com/assets/public/vegimg/ladisfing1.jpg" className="card-img-top" alt="..." />
+                            <div className='absolute'>
+                                <button className='btn'>41% OFF</button>
+                                <div>
+                                    <input type='radio'></input>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='desc'>
+                            <button className='patch'><i class="fa fa-bicycle" aria-hidden="true"></i> 2hrs</button>
+                            <p>Fresho</p>
+                            <h5>Cucumber - (loose)</h5>
+                            <select className="form-select mt-5" aria-label="Default select example">
+                                <option selected>1 Kg</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                            <div className='d-flex gap-2 mt-2'>
+                                <h4><i class="fa fa-inr" aria-hidden="true"></i> 68</h4>
+                                <p className='mt-1' style={{ textDecoration: " line-through" }}><i class="fa fa-inr" aria-hidden="true"></i> 68790</p>
+                            </div>
+                            <select className="form-select mt-4" aria-label="Default select example">
+                                <option selected><i class="fa fa-percent" aria-hidden="true"></i> Get it for <i class="fa fa-inr" aria-hidden="true"></i>49.40!</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                            <div className='d-flex'>
+                                <span style={{ border: '1px solid lightgrey', padding: '8px', marginTop: "8px" }}>
+                                    <i className="fa fa-bookmark-o" aria-hidden="true"></i>
+                                </span>
+                                <button className='btn1'>Add</button>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="col-md-3 shadow">
+                    <div className="card h-100">
+                        <div className='relative'>
+                            <img src="https://4.imimg.com/data4/AY/TE/IMOB-27708947/c__data_users_defapps_appdata_internetexplorer_temp_sav.jpg" className="card-img-top" alt="..." />
+                            <div className='absolute'>
+                                <button className='btn'>41% OFF</button>
+                                <div>
+                                    <input type='radio'></input>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='desc'>
+                            <button className='patch'><i class="fa fa-bicycle" aria-hidden="true"></i> 2hrs</button>
+                            <p>Fresho</p>
+                            <h5>Carrate - (loose)</h5>
+                            <select className="form-select mt-5" aria-label="Default select example">
+                                <option selected>1Pc - (approxi, 400 to 600 g)</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                            <div className='d-flex gap-2 mt-2'>
+                                <h4><i class="fa fa-inr" aria-hidden="true"></i> 68</h4>
+                                <p className='mt-1' style={{ textDecoration: " line-through" }}><i class="fa fa-inr" aria-hidden="true"></i> 68790</p>
+                            </div>
+                            <select className="form-select mt-4" aria-label="Default select example">
+                                <option selected><i class="fa fa-percent" aria-hidden="true"></i> Get it for <i class="fa fa-inr" aria-hidden="true"></i>49.40!</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                            <div className='d-flex'>
+                                <span style={{ border: '1px solid lightgrey', padding: '8px', marginTop: "8px" }}>
+                                    <i className="fa fa-bookmark-o" aria-hidden="true"></i>
+                                </span>
+                                <button className='btn1'>Add</button>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="col-md-3 shadow">
+                    <div className="card h-100">
+                        <div className='relative'>
+                            <img src="https://thumbs.dreamstime.com/b/vegetables-1430407.jpg" className="card-img-top" alt="..." />
+                            <div className='absolute'>
+                                <button className='btn'>41% OFF</button>
+                                <div>
+                                    <input type='radio'></input>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='desc'>
+                            <button className='patch'><i class="fa fa-bicycle" aria-hidden="true"></i> 2hrs</button>
+                            <p>Fresho</p>
+                            <h5>Capcium - (loose)</h5>
+                            <select className="form-select mt-5" aria-label="Default select example">
+                                <option selected>1 Kg</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                            <div className='d-flex gap-2 mt-2'>
+                                <h4><i class="fa fa-inr" aria-hidden="true"></i> 68</h4>
+                                <p className='mt-1' style={{ textDecoration: " line-through" }}><i class="fa fa-inr" aria-hidden="true"></i> 68790</p>
+                            </div>
+                            <select className="form-select mt-4" aria-label="Default select example">
+                                <option value="49.40" selected>&#xf002; Get it for &#x20b9;49.40!</option>
+                                <option value="1">&#xf002; One</option>
+                                <option value="2">&#xf002; Two</option>
+                                <option value="3">&#xf002; Three</option>
+                            </select>
+                            <div className='d-flex'>
+                                <span style={{ border: '1px solid lightgrey', padding: '8px', marginTop: "8px" }}>
+                                    <i className="fa fa-bookmark-o" aria-hidden="true"></i>
+                                </span>
+                                <button className='btn1'>Add</button>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>            </div>
+        </div>
+    );
+}
+
+export default Vivimart;
